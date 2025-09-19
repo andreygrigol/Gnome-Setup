8f84fb4fd6a7ab028207d50a17cbf3001e235776
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2025-09-19 12:20:10 -0300

    Now it's cloning and setting up the Gruvbox components such as GTK and Icons

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 
 mkdir ~/.local/share/icons
 
+if command -v pacman >/dev/null 2>&1; then
+	sudo pacman -S kitty fish tmux neovim nodejs npm python-virtualenv rofi starship
+elif command -v apt >/dev/null 2>&1; then
+	sudo apt install kitty fish tmux nodejs npm python3-virtualenv rofi
+else
+	echo "Could not identify which package manager is using!"
+fi
+
 DOCS_DIR=$(xdg-user-dir DOCUMENTS)
 mkdir "$DOCS_DIR/Gruvbox"
 
@@ -18,3 +26,8 @@ fi
 
 git clone https://github.com/SylEleuth/gruvbox-plus-icon-pack.git "$DOCS_DIR/Gruvbox/gruvbox-plus-icon-pack"
 git clone https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme "$DOCS_DIR/Gruvbox/Gruvbox-GTK-Theme"
+
+cd $DOCS_DIR/Gruvbox/Gruvbox-GTK-Theme/themes/
+./install.sh -t orange -c dark -l --tweaks soft float macos
+
+cp -r $DOCS_DIR/Gruvbox/gruvbox-plus-icon-pack/Gruvbox-Plus-Dark ~/.local/share/icons/
